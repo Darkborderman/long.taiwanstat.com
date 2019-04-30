96f200a858ad565fc50e35a65188617c3e90717a
darkborderman <reastw1234@gmail.com>
2019-04-30 21:44:37 +0800

    Add youth-jobs for project demo

---
+++ youth-jobs/assets/js/lineGraph.js
@@ -0,0 +1,231 @@
+let Linegraph={
+    //generate empty SVG element
+    generateGraph(format,container){
+        let svg=d3.select(container).append(`svg`)
+        .attr(`width`, format.width)
+        .attr(`height`, format.height);
+
+        let graph=svg.append(`g`)
+            .attr(`transform`, `translate(${format.margin.left},${format.margin.top})`);
+
+        graph.name=container;
+
+        return graph;
+    },
+    //append Y axis on SVG
+    generateYAxis(graph,data,height,unit){
+        let max= Math.max.apply(Math, data.map(function(o) { return o[`value`]; }));
+        let min= Math.min.apply(Math, data.map(function(o) { return o[`value`]; }));
+    
+        let y=d3.scaleLinear().domain([min,max]).range([height,0]);
+    
+        graph.append(`g`)
+            .attr(`class`, `y axis`)
+            .attr(`transform`, `translate(-5,0)`)
+            .call(d3.axisLeft(y).ticks(6));
+    
+        graph.append(`g`)
+            .attr(`class`, `y unit`)
+            .append(`text`)
+            .attr(`y`, -10)
+            .attr(`x`, -50)
+            .text(`${unit}`);
+        
+        return y;
+    },
+    //append X axis on SVG
+    generateXAxis(graph,data,width,height,unit){
+        let max= Math.max.apply(Math, data.map(function(o) { return o[`year`]; }));
+        let min= Math.min.apply(Math, data.map(function(o) { return o[`year`]; }));
+    
+        let x=d3.scaleLinear().domain([min-0.2,max+0.2]).range([0,width-10]);
+    
+        graph.append(`g`)
+            .attr(`class`, `x axis`)
+            .attr(`transform`, `translate(0,${height+2})`)
+            .call(d3.axisBottom(x).ticks(6,``));
+    
+        graph.append(`g`)
+            .attr(`class`, `x unit`)
+            .append(`text`)
+            .attr(`y`, 310)
+            .attr(`x`, width)
+            .text(`${unit}`);
+    
+        return x;
+    },
+    //append dot on graph, reutrn dot generated
+    generateDot(data,graph,x,y){
+        graph.append(`g`)
+        .attr(`class`,`dot graph`)
+        .selectAll(`circle`)
+        .data(data)
+        .enter()
+        .append(`circle`)
+        .attr(`class`,(d)=>{return `${d[`year`]} ${d[`type`]}`;})
+        .attr(`fill`,(d)=>{return Setting.color[d[`type`]];})
+        .attr(`opacity`,(d)=>{
+            if(d[`type`]==`待遇高`||d[`type`]==`工作穩定`||d[`type`]==`工作負擔較輕`)
+            return Setting.circle.strong.default.opacity;
+            else return Setting.circle.normal.default.opacity;
+        })
+        .attr(`r`,(d)=>{
+            if(d[`type`]==`待遇高`||d[`type`]==`工作穩定`||d[`type`]==`工作負擔較輕`)
+            return Setting.circle.strong.default.radius;
+            else return Setting.circle.normal.default.radius;
+        })
+        .attr(`cx`,(d)=>{return x(d[`year`]);})
+        .attr(`cy`,(d)=>{return y(d[`value`]);});
+
+        return graph.selectAll(`circle`);
+    },
+    //append line on graph, return line generated
+    generateLine(lineData,graph,x,y){
+        let valueline = d3.line()
+        .x(function(d) { return x(d[`year`]); })
+        .y(function(d) { return y(d[`value`]); });
+
+        for(let i=0;i<lineData.typeKind;i++)
+        {
+            graph.append(`g`)
+            .attr(`class`,`line graph`)
+            .data([lineData[i]])
+            .append(`path`)
+            .attr(`class`,(d)=>{return `${d[`type`]} line`;})
+            .attr(`stroke`,(d)=>{return Setting.color[d[`type`]]})
+            .attr(`stroke-width`,(d)=>{
+                if(d[`type`]==`待遇高`||d[`type`]==`工作穩定`||d[`type`]==`工作負擔較輕`)
+                return Setting.line.strong.default.strokeWidth;
+                else return Setting.line.normal.default.strokeWidth;
+            })
+            .attr(`opacity`,(d)=>{
+                if(d[`type`]==`待遇高`||d[`type`]==`工作穩定`||d[`type`]==`工作負擔較輕`)
+                return Setting.line.strong.default.opacity;
+                else return Setting.line.normal.default.opacity;
+            })
+            .attr(`fill`,`none`)
+            .attr(`d`,valueline);
+        }
+
+        return graph.selectAll(`.line path`);
+    },
+    appendbar(data,graph,x){
+        console.log(data);
+        graph.append(`g`)
+        .attr('class','bar graph')
+        .selectAll(`rect`)
+        .data(data)
+        .enter()
+        .append(`rect`)
+        .attr('pointer-events', 'none') //prevent trigger mouse event before duration
+        .transition()
+        .duration(1000)
+        .attr("x",(d)=>{ return x(d[`year`])-3})
+        .attr("y",(d)=>{ return 0})
+        .attr("height",303)
+        .attr("width",6)
+        .attr("fill","blue")
+        .attr("opacity",0.2)
+        .transition()
+        .attr('pointer-events','auto');
+
+        return graph.selectAll(`rect`);
+    },
+    //append hint text when hover
+    generateTooltip(d,index,graph,x,y,unit){
+        //out of bound index exception
+        if((graph.name==`#display`&&index%6==5))
+        {
+            graph.append(`text`)
+            .attr(`id`,`tooltip`)
+            .attr(`class`,`${d[`type`]}`)
+            .attr(`x`,()=>{return x(d[`year`])-10})
+            .attr(`y`,()=>{return y(d[`value`])+5})
+            .style(`stroke`,()=>{return Setting.color[d.type]})
+            .style(`stroke-width`, `1px`)
+            .attr(`fill`,()=>{return Setting.color[d.type]})
+            .text(`${d[`type`]} ${d[`value`]} ${unit}`)
+            .attr(`text-anchor`,`end`);
+        }
+        else{
+            graph.append(`text`)
+            .attr(`id`,`tooltip`)
+            .attr(`class`,`${d[`type`]}`)
+            .attr(`x`,()=>{return x(d[`year`])+10})
+            .attr(`y`,()=>{return y(d[`value`])+10})
+            .style(`stroke`,()=>{return Setting.color[d.type]})
+            .style(`stroke-width`, `1px`)
+            .attr(`fill`,()=>{return Setting.color[d.type]})
+            .text(`${d[`type`]} ${d[`value`]} ${unit}`)
+            .attr(`text-anchor`,`start`);
+        }
+    },
+    //remove hint point when leave
+    removeTooltip(){
+        d3.select(`#tooltip`).remove();
+    },
+    //find the length of categories and return with a 2D array
+    formatLineData(data){
+        let lineData=[],typeLength=0;
+        for(item in data){
+            if(data[item][`type`]==data[0][`type`]) typeLength++;
+        }
+        for(let i=0;i<(data.length/typeLength);i++){
+            lineData[i]=data.slice(i*typeLength,(i+1)*typeLength);
+            lineData[i].type=data[i*typeLength].type;
+        }
+        lineData.typeLength=typeLength;
+        lineData.typeKind=data.length/typeLength;
+        return lineData;
+    },
+    //generate info icon on grpah,return info
+    generateInfo(graph,width,lineData,container){
+        //clear inner content and re-render
+        document.getElementById(container).innerHTML=
+`本圖為青年勞工初次尋職時選擇工作的考慮因素折線圖
+用意為當有多種選擇時,青年勞工會以什麼當作最主要的考慮因素
+選項共有11種(選項可複選,故總和比率超過100%)`;
+        for(i=0;i<lineData.typeKind;i++){
+            let div=document.createElement(`div`);
+            div.innerHTML=lineData[i].type;
+            div.style.color=Setting.color[lineData[i].type];
+            document.getElementById(container).appendChild(div);
+        }
+        let info=graph.append(`g`).append(`image`)
+        .attr(`xlink:href`, `assets/svg/info.svg`)
+        .attr(`width`, 30)
+        .attr(`height`, 30)
+        .attr(`x`,width)
+        .attr(`y`,-30);
+        return info;
+    },
+    formatYearData(data,year){
+        let result = data.map(item =>{
+            if(item[`year`]==year) return item;
+        });
+    
+        result=result.filter(item=> {
+            return item != undefined;
+        });
+        return result;
+    },
+    generateCheckboxEvent(yearData){
+        for(let i=0;i<=10;i++)
+        {
+            d3.select(`#box-${yearData[i][`type`]}`)
+            .on(`click`,()=>{
+                if(document.getElementById(`box-${yearData[i][`type`]}`).checked){
+                    d3.selectAll(`.${yearData[i][`type`]}`)
+                    .attr('display','block')
+                    .attr('opacity',1);
+                }
+                else{
+                    d3.selectAll(`.${yearData[i][`type`]}`)
+                    .attr('display','none')
+                    .attr('opacity',0);
+                }
+            });
+        
+        }
+    }
+}
---
+++ youth-jobs/assets/js/setting.js
@@ -0,0 +1,99 @@
+let Setting={
+    graph:{
+        width:window.screen.width<1080?window.screen.width:window.screen.width*0.5,
+        height:400,
+        margin:{
+            top: 50,
+            right: 50,
+            bottom: 50,
+            left: 50
+        },
+        innerWidth:function(){
+            return this.width-this.margin.left-this.margin.right;
+        },
+        innerHeight:function(){
+            return this.height-this.margin.top-this.margin.left;
+        },
+    },
+    innerGraph:{
+        width:400,
+        height:400,
+        margin:{
+            top: 50,
+            right: 50,
+            bottom: 50,
+            left: 50
+        },
+        innerWidth:function(){
+            return this.width-this.margin.left-this.margin.right;
+        },
+        innerHeight:function(){
+            return this.height-this.margin.top-this.margin.left;
+        },
+    },
+    circle:{
+        strong:{
+            hover:{
+                radius:12,
+                opacity:1
+            },
+            default:{
+                radius:7,
+                opacity:0.7
+            },
+        },
+        normal:{
+            hover:{
+                radius:12,
+                opacity:0.7
+            },
+            default:{
+                radius:7,
+                opacity:0.4
+            },
+        }
+    },
+    line:{
+        strong:{
+            hover:{
+                strokeWidth:2,
+                opacity:1,
+            },
+            default:{
+                strokeWidth:2,
+                opacity:0.7,
+            },
+        },
+        normal:{
+            hover:{
+                strokeWidth:2,
+                opacity:0.7,
+            },
+            default:{
+                strokeWidth:2,
+                opacity:0.4,
+            },
+        },
+    },
+    color:{
+        //category 考慮因素
+            工作穩定:`#006400`,
+            待遇高:`#FF0000`,
+            工作負擔較輕:`#0000FF`,
+            有發展前景:`#10ddc2`,
+            通勤方便:`#7dace4`,
+            符合自己興趣:`#fc5c9c`,
+            能學以致用:`#8c82fc`,
+            能學習到知識:`#455d7a`,
+            有挑戰性:`#de95ba`,
+            有升遷機會:`#ff7c38`,
+            其他:`#5e63b6`,
+        //category 學歷
+            碩士及以上:`#006400`,
+            大學:`#FF0000`,
+            專科:`#8c82fc`,
+            高中職:`#de95ba`,
+            國中及以下:`#ff7c38`,
+            平均薪資:`#0000FF`,
+    }
+}
